dc5967b2af1e9273c6f79ae5f81b987644710f33
SajidSojib <sajidahmed2401@gmail.com>
2025-07-14 20:58:55 +0600

    set up the besic server and added cors, express, dotenv, mongodb packages

---
+++ index.js
@@ -0,0 +1,17 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 9000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('hellow from true review server');
+})
+
+app.listen(port, () => {
+    console.log(`listening to port ${port}`);
+})
