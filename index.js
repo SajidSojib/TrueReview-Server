48cb5f98a1cf365b624687ffa7f2cdb169207d80
SajidSojib <sajidahmed2401@gmail.com>
2025-07-17 02:22:31 +0600

    added post method to store new service

--- index.js
+++ index.js
@@ -27,16 +27,24 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
+
+    const serviceCollection = client.db('trueReview').collection('services');
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
+
+    app.post('/services', async (req, res) => {
+        const post = req.body;
+        const result = await serviceCollection.insertOne(post);
+        res.send(result);
+    })
+
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
