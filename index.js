51b0980346bb8fb1eca960e5a2ba1b9a1c2e423c
SajidSojib <sajidahmed2401@gmail.com>
2025-07-19 06:45:00 +0600

    implimented jwt

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 9000;
+const admin = require("firebase-admin");
+const serviceAccount = require("./firebase-admin-key.json");
 
 app.use(cors());
 app.use(express.json());
@@ -25,6 +27,43 @@ const client = new MongoClient(uri, {
   },
 });
 
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+
+const verifyTokenEmail = (req, res, next) => {
+    if(req?.query?.email) {
+      if (req.decoded.email !== req.query.email) {
+          return res.status(403).send({ error: true, message: 'forbidden access' })
+      }
+    }
+    next();
+}
+
+const verifyFirebaseToken = async (req, res, next) => {
+    if(req?.query?.email || req.method !== 'GET') {
+        const authorization = req.headers.authorization;
+        if (!authorization || !authorization.startsWith("Bearer ")) {
+          return res
+            .status(401)
+            .send({ error: true, message: "unauthorized access" });
+        }
+        const token = authorization.split(" ")[1];
+        try {
+          const decoded = await admin.auth().verifyIdToken(token);
+          req.decoded = decoded;
+          next();
+        } catch (error) {
+          return res
+            .status(401)
+            .send({ error: true, message: "unauthorized access" });
+        }
+    } else {
+        next();
+    }
+}
+
 async function run() {
   try {
     await client.connect();
@@ -39,7 +78,7 @@ async function run() {
 
 
 
-    app.get('/services', async (req, res) => {
+    app.get('/services', verifyFirebaseToken, verifyTokenEmail, async (req, res) => {
         const { email } = req.query;
         const query = email ? { email: email } : {};
         const result = await serviceCollection.find(query).toArray();
@@ -53,13 +92,13 @@ async function run() {
         res.send(result);
     })
 
-    app.post('/services', async (req, res) => {
+    app.post('/services', verifyFirebaseToken, async (req, res) => {
         const post = req.body;
         const result = await serviceCollection.insertOne(post);
         res.send(result);
     })
 
-    app.put('/services/:id', async (req, res) => {
+    app.put('/services/:id', verifyFirebaseToken, async (req, res) => {
         const id = req.params.id;
         const updatedService = req.body;
         const query = { _id: new ObjectId(id) }
@@ -70,18 +109,22 @@ async function run() {
         res.send(result);
     })
 
-    app.delete('/services/:id', async (req, res) => {
+    app.delete('/services/:id', verifyFirebaseToken, async (req, res) => {
         const id = req.params.id;
         const query = { _id: new ObjectId(id) }
         const result = await serviceCollection.deleteOne(query);
         res.send(result);
     })
 
-    app.get('/reviews', async (req, res) => {
+    app.get('/limitedServices', async (req, res) => {
+        const result = await serviceCollection.find().limit(6).toArray();
+        res.send(result);
+    })
+
+    app.get('/reviews', verifyFirebaseToken, verifyTokenEmail, async (req, res) => {
         const { email } = req.query;
         const query = email ? { email: email } : {};
         const result = await reviewCollection.find(query).toArray();
-        console.log(query);
 
         res.send(result);
     })
@@ -93,13 +136,13 @@ async function run() {
         res.send(result);
     })
 
-    app.post('/reviews', async (req, res) => {
+    app.post('/reviews', verifyFirebaseToken, async (req, res) => {
         const post = req.body;
         const result = await reviewCollection.insertOne(post);
         res.send(result);
     })
 
-    app.patch('/reviews/:id', async (req, res) => {
+    app.patch('/reviews/:id', verifyFirebaseToken, async (req, res) => {
         const id = req.params.id;
         const updatedReview = req.body;
         const query = { _id: new ObjectId(id) }
@@ -110,7 +153,7 @@ async function run() {
         res.send(result);
     })
 
-    app.delete('/reviews/:id', async (req, res) => {
+    app.delete('/reviews/:id', verifyFirebaseToken, async (req, res) => {
         const id = req.params.id;
         const query = { _id: new ObjectId(id) }
         const result = await reviewCollection.deleteOne(query);
